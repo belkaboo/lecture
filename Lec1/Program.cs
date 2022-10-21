7bafc5219fb1b141b26a488a539cad02bd3768db
belka.boo <ibelka.boo@gmail.com>
2022-10-21 15:37:21 +0300

    "int/double@

--- Lec1/Program.cs
+++ Lec1/Program.cs
@@ -8,10 +8,27 @@ Console.WriteLine("Hello, World!");
 
 
 
-
+/*
 Console.WriteLine("Enter your name");
 string username = Console.ReadLine(); //  ввод переменной и считывание из консоли
 Console.Write("Hi, "); // вывод в консоль( WriteLine - в одну линию т.е. будет пренос строки)
 Console.WriteLine(username); // вывод значения переменной username
+*/
+
+/*
+int number1 = 12;
+int number2 = 5; 
+int result = (number1 / number2); //  result = 2 т.к. типа переменной int - целые числа.
+Console.WriteLine(result);
+*/
+
+/*
+double number1 = 12; // double -  вещественные числа
+double number2 = 5; 
+double result = (number1 / number2); // result = 2,4
+Console.WriteLine(result);
+*/
+
+
 
 

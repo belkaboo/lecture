b12ecf7c422a0452956c6f3fd5848ae819df78f0
belka.boo <ibelka.boo@gmail.com>
2022-10-21 15:25:24 +0300

    example1,2

---
+++ Lec1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Код из первой лекции. Сними комментарии с блоков перед использованием
+
+/*
+Console.Clear(); // очистка консоли
+Console.WriteLine("Hello, World!");
+*/
+
+
+
+
+
+Console.WriteLine("Enter your name");
+string username = Console.ReadLine(); //  ввод переменной и считывание из консоли
+Console.Write("Hi, "); // вывод в консоль( WriteLine - в одну линию т.е. будет пренос строки)
+Console.WriteLine(username); // вывод значения переменной username
+
+

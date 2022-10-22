5a2540cb55853f4569d9b9b9386e5b09210601a9
belka.boo <ibelka.boo@gmail.com>
2022-10-22 03:41:03 +0300

    array method

--- Lec2/Program.cs
+++ Lec2/Program.cs
@@ -1,17 +1,50 @@
-﻿int[] array = {1, 12, 2,3,4,23,1,2,};
-int n = array.Length;
-int find =2;
-
-int index = 0;
-while (index < n)
+﻿void FillArray(int[] collection)
 {
-    if (array[index] == find)
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
     {
-        
-        Console.WriteLine(index);
-        break;
+        collection[index] = new Random().Next(1,20);
+        index++;
     }
-
-    index++;
 }
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+          position = index;
+          break;
+        }
+      index++;  
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
+
 

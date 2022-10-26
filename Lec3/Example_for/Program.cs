2ac180e256e4429dc3b03ae2ff159120550f2f16
belka.boo <ibelka.boo@gmail.com>
2022-10-26 16:11:35 +0300

    replacemethod

---
+++ Lec3/Example_for/Program.cs
@@ -0,0 +1,12 @@
+﻿string MethodFor(int count, string text)
+{
+    
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = MethodFor(10, "textFor");
+Console.WriteLine(res);

f70f22db2de9cd02df05af0e193f89f722f9d7c3
belka.boo <ibelka.boo@gmail.com>
2022-11-30 03:47:35 +0300

    Lec_5

---
+++ Lec5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+{
+
+}
+Console.Clear();
+
+int a = 12;
+Console.WriteLine(a.GetType()); // определение типа переменной
+
+var b = 12;
+Console.WriteLine(b.GetType().Name); // вывод только типа переменной
+
+
+// Пример необходимости var
+var data = new int[] { 1, 2, 3, 4, }
+        .Where(e => e > 0)
+        .Select(e => new { q = e, w = e + 1 });
+
+Console.WriteLine(data.GetType().Name);
+
+
+
+
